8a966e58536505b3383395622f1182ba37213e15
Diyorbek Burkhanov <98954404+burkhanovDiyorbek@users.noreply.github.com>
2024-09-26 13:04:21 +0500

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,13 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
-  apiKey: 'AIzaSyAuOA5J54sSMr59g4Qc0ylxaisGTxdnCIk',
-  authDomain: 'president-school-8273b.firebaseapp.com',
-  projectId: 'president-school-8273b',
-  storageBucket: 'president-school-8273b.appspot.com',
-  messagingSenderId: '961384384206',
-  appId: '1:961384384206:web:2b5113094761cbe16cf29f',
-  measurementId: 'G-24MHLM0KYR',
+  apiKey: "AIzaSyDTIcrd7p2gll1F6wyr-rdoHG0F5fwPxko",
+  authDomain: "qarshiimi-412d4.firebaseapp.com",
+  projectId: "qarshiimi-412d4",
+  storageBucket: "qarshiimi-412d4.appspot.com",
+  messagingSenderId: "243683096377",
+  appId: "1:243683096377:web:b192a426a2ba57e94b9ae8"
 };
 
 const app = initializeApp(firebaseConfig);
